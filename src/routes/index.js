6982ec64a437fd9c002ebcf49f84ffb8c2d9865c
priscilatc <priscila.tinoco8802@est.uca.edu.ni>
2021-05-03 16:17:00 -0600

    segundo commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 
-const {getPhotos, getPhotosByColor, welcome} = require('../controllers/index.controller')
+const {getPhotos, getPhotosByColor, welcome} = require('../controllers/index.controller,js')
 
 router.get('/', welcome);
 router.get('/photos', getPhotos);
